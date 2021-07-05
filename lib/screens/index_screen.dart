b034b86b339041220cd155c9ea2e7391b0c31d98
domenicYR <domenic.rosenbaum54@gmail.com>
2021-07-05 13:46:55 +0200

    design bearbeitet

--- lib/screens/index_screen.dart
+++ lib/screens/index_screen.dart
@@ -21,75 +21,146 @@ class IndexScreen extends StatelessWidget {
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(10.0),
                       child: Image.asset(
-                        "assets/images/las_vegas.jpg",
+                        "assets/images/munich.jpg",
                       ),
                     ),
                   ),
                 ],
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Unsere Städte',
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          decorationThickness: 2.85,
-                          decorationColor: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        color: Colors.deepOrange,
+             
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          'Unsere Städte',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            color: Colors.deepOrange,
+                          ),
+                        ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.all(10),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.asset(
-                                'assets/images/las_vegas.jpg',
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.asset(
+                                  'assets/images/frankfurt.png',
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Text('Was haben unsere Städte zu bieten',
-                                  style: TextStyle(fontSize: 15)),
-                              Padding(
-                                padding: const EdgeInsets.all(9.0),
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ButtonStyle(
-                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrange),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Text('Lass dich inspirieren',
+                                    style: TextStyle(fontSize: 15)),
+                                Padding(
+                                  padding: const EdgeInsets.all(9.0),
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrange),
+                                      ),
+                                      child: Text('Zu den Städten'),
                                     ),
-                                    child: Text('Zu den Städten'),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          'Deine Städte',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.deepOrange,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.asset(
+                                  'assets/images/hamburg.png',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Text('Füge eine Stadt hinzu',
+                                    style: TextStyle(fontSize: 15)),
+                                Padding(
+                                  padding: const EdgeInsets.all(9.0),
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => AddCityScreen()),
+                                        );
+                                      },
+                                      style: ButtonStyle(
+                                        backgroundColor: MaterialStateProperty.all<Color>(Colors.deepOrange),
+                                      ),
+                                      child: Text('Zu den Städten'),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+
+
+
+
+
+              /*
               Card(
                 child: Column(
                   children: <Widget>[
                     Text(
                       'Deine Städte',
                       style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          decorationThickness: 2.85,
-                          decorationColor: Colors.black,
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                         color: Colors.deepOrange,
@@ -104,7 +175,7 @@ class IndexScreen extends StatelessWidget {
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(10.0),
                               child: Image.asset(
-                                'assets/images/las_vegas.jpg',
+                                'assets/images/hamburg.png',
                               ),
                             ),
                           ),
@@ -140,6 +211,7 @@ class IndexScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              */
             ]),
           )),
     );
